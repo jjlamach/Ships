a3db7902a4139bc8d907e0424d3746352d75531e
Julio L <32489610+jlama94@users.noreply.github.com>
2018-04-10 18:44:07 -0500

    Update OceanMap.java

--- src/Columbus/OceanMap.java
+++ src/Columbus/OceanMap.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Random;
 import java.awt.Point;
 /**
- * @author Julio Lama<br/>
+ * @author Julio Lama
  */
 public class OceanMap {
 	int[][] oceanMap;
@@ -12,11 +12,7 @@ public class OceanMap {
 	int dimension;
 	int islandCount;
 	
-	/**
-	 *  Builds an ocean map with a specified dimension and place the number of islands given. <br/>
-	 * @param dimension
-	 * @param islands
-	 */
+
 	public OceanMap(int dimension, int islands) {
 		rand = new Random();
 		this.dimension = dimension;
@@ -36,19 +32,12 @@ public class OceanMap {
 		}
 	}
 	
-	/**
-	 * 
-	 * @return The whole ocean grid.<br/>
-	 */
+
 	public int[][] getMap() {
 		return oceanMap;
 	}
 	
-	/**
-	 * Looks for all the ones in the map.<br/>
-	 * This helped me place them on the grid. So that I don't create imageViews many times.
-	 * @return the list of all the coordinates of the islands.<br/>
-	 */
+
 	public List<Point> getIslands() {
 		List<Point> islands = new ArrayList<Point>();
 		for(int i = 0; i < oceanMap.length; i++) {
